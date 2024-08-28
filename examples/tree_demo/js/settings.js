8bb2aac2c2bd0d8a75f21db996bdc1873ecfb462
nvidia <nvidia@localhost.localdomain>
2024-08-28 09:24:57 +0000

    first commit

---
+++ examples/tree_demo/js/settings.js
@@ -0,0 +1,20 @@
+const DEFAULT_WS_URL =  "ws://0.0.0.0:7860/ws" // || "ws://172.23.170.73:7860/ws"
+const DEFAULT_PROMPT = "[a phone][a glasses][a mask][a hand]"
+const DEFAULT_OBJECT = [
+  {
+    "promptName": "cell phone",
+    "displayName": "cell phone",
+  },
+  {
+    "promptName": "glasses",
+    "displayName": "glasses",
+  },
+  {
+    "promptName": "mask",
+    "displayName": "mask",
+  },
+  {
+    "promptName": "hand",
+    "displayName": "hand",
+  }
+]
