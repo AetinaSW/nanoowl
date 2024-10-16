cfe0e93b03339eee3f8f977c96766656dbbd4f46
Aetina-Tim <92358191+Aetina-Tim@users.noreply.github.com>
2024-10-16 16:57:07 +0800

    Update start_service.sh

--- start_service.sh
+++ start_service.sh
@@ -2,9 +2,9 @@
 
 # start nanoowl ui service
 /home/nvidia/nanoowl_aetina/ui/docker-run.sh 
-firefox http://0.0.0.0:7861
+firefox --kiosk http://0.0.0.0:7861 > /dev/null 2>&1 &
 
 # run nanoowl service
 jetson-containers run -v /home/nvidia/nanoowl_aetina:/opt/nanoowl_aetina --workdir /opt/nanoowl_aetina/examples/tree_demo   $(autotag nanoowl)     python3 tree_demo.py /opt/nanoowl/data/owl_image_encoder_patch32.engine
 
-
+pkill -f firefox > /dev/null 2>&1
